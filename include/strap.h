50f980474c8c490538b3945dcfe4115a23a64c28
Harry Stafford <harrydstafford@gmail.com>
2022-08-20 18:38:05 +0300

    Replace add with insert and add more array funtions

--- include/strap.h
+++ include/strap.h
@@ -51,46 +51,49 @@ extern size_t      strap_array_count(const StrapArray *arr);
 extern StrapString *strap_array_create_string(const StrapArray *arr, size_t i);
 extern StrapString *strap_array_copy_string(const StrapArray *arr, size_t i, StrapString *str);
 extern StrapArray  *strap_array_append_string(StrapArray *arr, const StrapString *str);
-extern StrapArray  *strap_array_add_string(StrapArray *arr, size_t i, const StrapString *str);
+extern StrapArray  *strap_array_insert_string(StrapArray *arr, size_t i, const StrapString *str);
 extern StrapArray  *strap_array_replace_string(StrapArray *arr, size_t i, const StrapString *str);
 extern size_t      *strap_array_find_string(const StrapArray *arr, StrapString *str);
 
 extern const char  *strap_array_get_cstr(const StrapArray *arr, size_t i);
 extern StrapArray  *strap_array_append_cstr(StrapArray *arr, const char *cstr);
-extern StrapArray  *strap_array_add_cstr(StrapArray *arr, size_t i, const char *cstr);
+extern StrapArray  *strap_array_insert_cstr(StrapArray *arr, size_t i, const char *cstr);
 extern StrapArray  *strap_array_replace_cstr(StrapArray *arr, size_t i, const char *cstr);
 extern size_t      *strap_array_find_cstr(const StrapArray *arr, const char *cstr);
 
 extern short      *strap_array_get_short(const StrapArray *arr, size_t i);
 extern StrapArray *strap_array_append_short(StrapArray *arr, short num);
-extern StrapArray *strap_array_add_short(StrapArray *arr, size_t i, short num);
+extern StrapArray *strap_array_insert_short(StrapArray *arr, size_t i, short num);
 extern StrapArray *strap_array_replace_short(StrapArray *arr, size_t i, short num);
 extern size_t     *strap_array_find_short(const StrapArray *arr, short num);
 
 extern int        *strap_array_get_int(const StrapArray *arr, size_t i);
 extern StrapArray *strap_array_append_int(StrapArray *arr, int num);
-extern StrapArray *strap_array_add_int(StrapArray *arr, size_t i, int num);
+extern StrapArray *strap_array_insert_int(StrapArray *arr, size_t i, int num);
 extern StrapArray *strap_array_replace_int(StrapArray *arr, size_t i, int num);
 extern size_t     *strap_array_find_int(const StrapArray *arr, int num);
 
 extern long       *strap_array_get_long(const StrapArray *arr, size_t i);
 extern StrapArray *strap_array_append_long(StrapArray *arr, long num);
-extern StrapArray *strap_array_add_long(StrapArray *arr, size_t i, long num);
+extern StrapArray *strap_array_insert_long(StrapArray *arr, size_t i, long num);
 extern StrapArray *strap_array_replace_long(StrapArray *arr, size_t i, long num);
 extern size_t     *strap_array_find_long(const StrapArray *arr, long num);
 
 extern float      *strap_array_get_float(const StrapArray *arr, size_t i);
 extern StrapArray *strap_array_append_float(StrapArray *arr, float num);
-extern StrapArray *strap_array_add_float(StrapArray *arr, size_t i, float num);
+extern StrapArray *strap_array_insert_float(StrapArray *arr, size_t i, float num);
 extern StrapArray *strap_array_replace_float(StrapArray *arr, size_t i, float num);
 extern size_t     *strap_array_find_float(const StrapArray *arr, float num);
 
 extern double     *strap_array_get_double(const StrapArray *arr, size_t i);
 extern StrapArray *strap_array_append_double(StrapArray *arr, double num);
-extern StrapArray *strap_array_add_double(StrapArray *arr, size_t i, double num);
+extern StrapArray *strap_array_insert_double(StrapArray *arr, size_t i, double num);
 extern StrapArray *strap_array_replace_double(StrapArray *arr, size_t i, double num);
 extern size_t     *strap_array_find_double(const StrapArray *arr, double num);
 
+extern StrapArray  *strap_array_concat(StrapArray *arr1, const StrapArray *arr2);
+extern StrapArray  *strap_array_insert_array(StrapArray *arr1, size_t i, const StrapArray *arr2, size_t start, size_t n);
+extern StrapArray  *strap_array_clear(StrapArray *arr);
 extern StrapArray  *strap_array_create_subarray(const StrapArray *arr, size_t start, size_t n);
 extern StrapArray  *strap_array_remove(StrapArray *arr, size_t i);
 extern StrapArray  *strap_array_reverse(StrapArray *arr);
