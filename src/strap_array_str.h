d189158b13d22ffa1f6a55ea73806092a04a23e6
Harry Stafford <harrydstafford@gmail.com>
2022-10-21 01:15:46 +0300

    Update comments

--- src/strap_array_str.h
+++ src/strap_array_str.h
@@ -3,6 +3,7 @@
 
 #include "strap_internal.h"
 
+/* Macros used to access str_array content from StrapArray */
 #define str_sarr(arr) ((struct str_array*) arr->data)
 #define str_buf(arr) (str_sarr(arr)->buf)
 #define str_null(arr, idx) (str_sarr(arr)->nulls[idx])
@@ -22,16 +23,23 @@ do {                                                                            
 	}                                                                                   \
 } while (0)
 
-/*
-* nulls[] contains the index of the null terminating char of each element
-* from buf[], which is used for:
-*
-*   Getter: buf + nulls[i-1] + 1
-*   Length: nulls[i] - nulls[i-1]
-*
-* Note: the above will not apply for the first element, as it doesn't/can't
-* refer to the previous element.
-*/
+/**
+ * nulls[] contains the index of the null terminating char of each element
+ * from buf[], which is used for:
+ *
+ *   Getter: buf + nulls[i-1] + 1
+ *   Length: nulls[i] - nulls[i-1]
+ *
+ * Note: the above will not apply for the first element, as it doesn't/can't
+ * refer to the previous element. In general, these values and should be
+ * accessed and manipulated using the str macros defined above.
+ *
+ * buf[] contains the string elements separated by a null terminating char:
+ *
+ *   [ s t r 1 \0 s t r 2 \0 s t r i n g 3 \0 ]
+ *   count = 3, nulls = [4, 9, 17]
+ *
+ */
 struct str_array {
 	size_t buflen;
 	ushort *nulls;
@@ -43,6 +51,5 @@ extern int str_resize_buf(StrapArray *arr, size_t buflen);
 
 extern struct str_array* str_resize(StrapArray *arr,
 	size_t capacity, size_t buflen);
-
 
 #endif
